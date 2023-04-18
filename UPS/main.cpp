618b0f81ce0bdbfb639ea6efee99bf9b7c67886e
Tiffany <verycute.t@gmail.com>
2023-04-18 15:55:29 -0400

    add server

--- UPS/main.cpp
+++ UPS/main.cpp
@@ -1,19 +1,12 @@
-#include "connect_world.h"
+#include "server.h"
 
+#define PORT "12345"
 
 int main(){
-    int server_fd = setup_client("vcm-33209.vm.duke.edu", "12345");
-    google::protobuf::io::FileInputStream * in=new google::protobuf::io::FileInputStream(server_fd);
-    google::protobuf::io::FileOutputStream *out=new google::protobuf::io::FileOutputStream(server_fd);
-
-    UConnect connect;
-    connect.set_worldid(12345);
-    UInitTruck *truck=connect.add_trucks();
-    truck->set_id(1);
-    truck->set_x(2);
-    truck->set_y(3);
-    connect.set_isamazon(false);
-
-
+    const char * port = PORT;
+    Server * myServer = new Server(port);
+    myServer->run();
     return 0;
 }
+
+
